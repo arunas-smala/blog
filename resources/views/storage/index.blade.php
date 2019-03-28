f262dbdd5b42d18f8240484f00186f272ada2104
Arunas Smaliukas <arunas@smala.lt>
2019-03-28 15:05:43 +0200

    rand img

--- resources/views/storage/index.blade.php
+++ resources/views/storage/index.blade.php
@@ -7,7 +7,7 @@
         <div class="@if ($loop->first) first @endif">
             <div class="card @if ($loop->last) last @endif">
             <div class="card-body">
-                <img class="img-fluid" src="https://picsum.photos/600/400?v{{ $file['filename'] }}" />
+                <img class="img-fluid" src="https://picsum.photos/600/400?v{{ rand() }}" />
                 <h5 class="my-2">{{ $file['filename'] }}</h5>
                 <div>
                     {{ $file['created_at'] }}
